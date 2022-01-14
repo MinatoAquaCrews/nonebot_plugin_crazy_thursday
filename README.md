fa4cd5d8489a56a63756d02399266a958787dbe5
KafCoppelia <ubuntu@localhost.localdomain>
2022-01-14 16:00:09 +0800

    发布pip或poetry安装🥳

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# nonebot_plugin_crazy_thursday
-Crazy Thursday plugin for nonebot v2 from nonebot_instant_plugins
+<div align="center">
+
+# Crazy Thursday
+
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable-next-line MD036 -->
+_✨ 天天疯狂 ✨_
+<!-- prettier-ignore-end -->
+
+</div>
+
+<p align="center">
+  
+  <a href="https://github.com/KafCoppelia/nonebot_plugin_crazy_thursday/blob/main/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-informational">
+  </a>
+  
+  <a href="https://github.com/nonebot/nonebot2">
+    <img src="https://img.shields.io/badge/nonebot-v2-green">
+  </a>
+  
+  <a href="">
+    <img src="https://img.shields.io/badge/release-v0.1.0-orange">
+  </a>
+  
+</p>
+</p>
+
+## 版本
+
+v0.1.1
+
+## 安装
+
+1. 通过`pip`或`poetry`安装；
+
+2. 读取文案的默认路径位于`./resource`下；
+
+## 功能
+
+天天疯狂！随机输出KFC疯狂星期四文案。
+
+## 命令
+
+1. [疯狂/狂乱]星期[一|二|三|四|五|六|日]；
+2. 输入**疯狂星期八**等不合法时间将提示；
+
+## 本插件改自HoshinoBot疯狂星期四插件
+
+[HoshinoBot-fucking_crazy_thursday](https://github.com/Nicr0n/fucking_crazy_thursday)
---
+++ nonebot_plugin_crazy_thursday/__init__.py
@@ -0,0 +1,30 @@
+from nonebot.typing import T_State
+import random
+import json
+import os
+import re
+import nonebot
+from nonebot import logger
+from nonebot import on_command, on_regex
+from nonebot.adapters.cqhttp import Bot, GroupMessageEvent, GROUP
+
+_CRAZY_PATH = nonebot.get_driver().config.crazy_path
+CRAZY_PATH = os.path.join(os.path.dirname(__file__), "resource") if not _CRAZY_PATH else _CRAZY_PATH
+# crazy = on_command('疯狂星期四', aliases={"星期四", "KFC", "V我50"}, permission=GROUP, priority=15, block=True)
+crazy = on_regex(r'[疯狂|狂乱]星期.', permission=GROUP, priority=15, block=True)
+
+@crazy.handle()
+async def random_post(bot: Bot, event: GroupMessageEvent, state: T_State):
+    iscrazy = re.search(r'(.*?)星期[一|二|三|四|五|六|日]', event.get_plaintext())
+    crazy_day = iscrazy.group(0)[-3:] if iscrazy is not None else None
+    if crazy_day is None:
+        await crazy.finish("给个准确时间，OK?")
+        
+    # json数据存放路径
+    filePath = os.path.join(CRAZY_PATH, 'post.json')
+    # 将json对象加载到数组
+    kfc = json.load(open(filePath, 'r', encoding="UTF-8")).get('post')
+    # 随机选取数组中的一个对象
+    randomPost = random.choice(kfc)
+    randomPost = randomPost.replace("星期四", crazy_day)
+    await bot.send(event=event, message=randomPost)
